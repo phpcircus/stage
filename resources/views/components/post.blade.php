b0cc0103ccc97d8b83793e9f3f60952f786ab548
Clayton Stone <devcircus@outlook.com>
2021-05-26 11:14:00 -0500

    wip

--- resources/views/components/post.blade.php
+++ resources/views/components/post.blade.php
@@ -3,39 +3,14 @@
     'loop'
 ])
 @if($loop->iteration  == 1)
-    <section class="col-span-6 p-8 overflow-hidden font-sans text-gray-600 lg:grid lg:grid-cols-3 bg-white/25">
+    <section class="col-span-6 p-8 overflow-hidden font-sans text-gray-600 shadow lg:grid lg:grid-cols-3 lg:gap-4 bg-white/25">
         <!-- Post Image -->
-        <div class="w-full col-span-1">
+        <div class="w-full col-span-1 mb-2 lg:mb-0">
             <img src="{{ $post->primary_image }}" />
         </div>
         <!-- Post Categories and timestamp -->
-        <div class="flex flex-col justify-between">
-            <div class="flex flex-col space-y-2">
-                @foreach($post->categories as $category)
-                    <span
-                        class="w-1/4 inline-block py-0.5 px-2 mr-1 rounded bg-white text-gray-600 border border-gray-600 text-xs font-medium tracking-widest tag-wordpress">
-                        {{ $category->name }}
-                    </span>
-                @endforeach
-            </div>
-            <p class="text-xs italic text-gray-500">Published {{ $post->published_at->diffForHumans() }}</p>
-        </div>
-        </div>
-        <h2 class="my-2 text-2xl font-medium text-gray-900 title-font"><a
-                href="/posts/{{ $post->slug }}"
-                class="inline-block hover:underline">{{ $post->title }}</a>
-        </h2>
-        <p class="mb-4 leading-relaxed">{{ $post->summary }}</p>
-        <div class="flex flex-wrap items-center w-full pb-4 mt-auto">
-            <a href="/posts/{{ $post->slug }}"
-                class="inline-flex items-center text-gray-900 hover:underline">Read More
-            </a>
-        </div>
-    </section>
-@else
-    <section class="{{ $loop->iteration > 3 ? 'col-span-2' : 'col-span-3' }} p-8 overflow-hidden font-sans text-gray-600 bg-white/25">
-        <div class="flex flex-col items-start w-full">
-            <div class="mb-2">
+        <div class="flex flex-col col-span-2">
+            <div class="flex mb-2 space-x-2">
                 @foreach($post->categories as $category)
                     <span
                         class="inline-block py-0.5 px-2 mr-1 rounded bg-white text-gray-600 border border-gray-600 text-xs font-medium tracking-widest tag-wordpress">
@@ -43,20 +18,42 @@
                     </span>
                 @endforeach
             </div>
-            <div class="w-full">
-                <img src="{{ $post->primary_image }}" />
-            </div>
-            <p class="mt-2 text-xs italic text-gray-500">Published {{ $post->published_at->diffForHumans() }}</p>
-            <h2 class="my-2 text-2xl font-medium text-gray-900 title-font"><a
+            <p class="mb-4 text-xs italic text-gray-500">Published {{ $post->published_at->diffForHumans() }}</p>
+            <h2 class="self-start my-2 text-2xl font-medium text-gray-900 title-font"><a
                     href="/posts/{{ $post->slug }}"
                     class="inline-block hover:underline">{{ $post->title }}</a>
             </h2>
             <p class="mb-4 leading-relaxed">{{ $post->summary }}</p>
-            <div class="flex flex-wrap items-center w-full pb-4 mt-auto">
-                <a href="/posts/{{ $post->slug }}"
-                    class="inline-flex items-center text-gray-900 hover:underline">Read More
-                </a>
-            </div>
+            <a href="/posts/{{ $post->slug }}"
+                class="inline-flex items-center self-end text-gray-900 hover:underline">Read More
+            </a>
         </div>
     </section>
+@else
+<section class="{{ $loop->iteration < 4 ? 'col-span-3' : 'col-span-2' }} p-8 overflow-hidden font-sans text-gray-600 shadow bg-white/25">
+    <div class="flex flex-col items-start w-full">
+        <div class="w-full mb-2">
+            <img src="{{ $post->primary_image }}" />
+        </div>
+        <div class="mb-2">
+            @foreach($post->categories as $category)
+                <span
+                    class="inline-block py-0.5 px-2 mr-1 rounded bg-white text-gray-600 border border-gray-600 text-xs font-medium tracking-widest tag-wordpress">
+                    {{ $category->name }}
+                </span>
+            @endforeach
+        </div>
+        <p class="text-xs italic text-gray-500">Published {{ $post->published_at->diffForHumans() }}</p>
+        <h2 class="my-2 text-2xl font-medium text-gray-900 title-font"><a
+                href="/posts/{{ $post->slug }}"
+                class="inline-block hover:underline">{{ $post->title }}</a>
+        </h2>
+        <p class="mb-4 leading-relaxed">{{ $post->summary }}</p>
+        <div class="flex flex-wrap items-center self-end w-full pb-4 mt-auto">
+            <a href="/posts/{{ $post->slug }}"
+                class="inline-flex items-center ml-auto text-gray-900 hover:underline">Read More
+            </a>
+        </div>
+    </div>
+</section>
 @endif
--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -43,13 +43,7 @@
                             @endif
                         @else
                             <span class="inline-flex rounded-md">
-                                <button type="button" class="inline-flex items-center px-3 py-2 text-sm font-medium leading-4 text-gray-500 transition bg-white border border-transparent rounded-md hover:text-gray-700 focus:outline-none">
-                                    Account
-
-                                    <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                                        <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-                                    </svg>
-                                </button>
+                                <x-heroicon-o-user-circle class="w-12 h-12 text-gray-600 transition border-2 border-transparent rounded-full cursor-pointer hover:text-gray-400 hover:border-red-500/25 focus:outline-none focus:border-gray-300" />
                             </span>
                         @endauth
                     </x-slot>
