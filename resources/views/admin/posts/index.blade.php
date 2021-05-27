98f0edf1f42a3b169913fad4109d8113761841ec
Clayton Stone <devcircus@outlook.com>
2021-05-27 00:56:57 -0500

    More work on post add & update form.

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -1,9 +1,7 @@
 <x-layouts.admin.page>
-    <x-slot name="primaryButton">
-        <button type="button"
-            class="inline-flex items-center px-4 py-2 ml-3 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-            New Post
-        </button>
+    <x-slot name="header">
+        {{ ucfirst(Str::after(request()->route()->uri, '/')) }}
     </x-slot>
-    <livewire:admin.posts.table />
+    <livewire:admin.posts.table key="posts-table-component" />
+    <livewire:admin.posts.form key="post-form-component" />
 </x-layouts.admin.page>
--- resources/views/components/layouts/admin/page.blade.php
+++ resources/views/components/layouts/admin/page.blade.php
@@ -4,11 +4,8 @@
             <div class="md:flex md:items-center md:justify-between">
                 <div class="flex-1 min-w-0">
                     <h2 class="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate">
-                        {{ ucfirst(Str::after(request()->route()->uri, '/')) }}
+                        {{ $header }}
                     </h2>
-                </div>
-                <div class="flex mt-4 md:mt-0 md:ml-4">
-                    {{ $primaryButton }}
                 </div>
             </div>
             <div class="mt-12">
