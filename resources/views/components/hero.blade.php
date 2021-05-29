b15e3a6e81a93357432190f1d1f6efc4f39a36b4
Clayton Stone <devcircus@outlook.com>
2021-05-29 11:17:21 -0500

    Make hero slightly taller on mobile.

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -1,6 +1,6 @@
 <header id="up" class="relative h-56 mx-auto bg-fixed bg-center bg-no-repeat bg-cover md:px-12 xl:px-0 md:h-96">
     <!-- Overlay Background + Center Control -->
-    <div class="flex justify-start h-48 md:h-80">
+    <div class="flex justify-start h-56 md:h-80">
         <div>
             <div class="mx-2 text-left transform -rotate-3 font-badhandwriting">
                 <h1 class="text-4xl font-semibold text-red-500 xs:text-7xl md:text-8xl">
