06a8482bd592e2d8eb88fa02c53be1adae9cb1fc
Clayton Stone <devcircus@outlook.com>
2021-05-28 23:58:13 -0500

    Page hero style tweaks. Post image size tweak.

--- resources/views/projects.blade.php
+++ resources/views/projects.blade.php
@@ -1,8 +1,8 @@
 <x-app-layout>
     <div class="py-12">
-        <header id="up" class="relative w-3/4 h-56 mx-auto bg-fixed bg-center bg-no-repeat bg-cover md:h-96">
+        <header id="up" class="relative w-3/4 h-24 mx-auto bg-fixed bg-center bg-no-repeat bg-cover md:h-56">
             <!-- Overlay Background + Center Control -->
-            <div class="flex justify-start h-48 md:h-80">
+            <div class="flex justify-start h-20 md:h-40">
                 <div>
                     <div class="mx-2 text-left transform -rotate-3 font-badhandwriting">
                         <h1 class="text-6xl font-semibold text-red-500 xs:text-7xl md:text-8xl">
