efb8b98217de2a0d6f609a207206ead6d8f3f2db
Clayton Stone <devcircus@outlook.com>
2021-05-29 11:14:35 -0500

    Tweaks for mid-size screens.

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <div class="px-6 py-12 mx-auto lg:px-0 max-w-7xl">
-        <header id="up" class="relative h-24 mx-auto bg-fixed bg-center bg-no-repeat bg-cover md:h-56">
+        <header id="up" class="relative h-24 mx-auto bg-fixed bg-center bg-no-repeat bg-cover md:px-12 xl:px-0 md:h-56">
             <!-- Overlay Background + Center Control -->
             <div class="flex justify-start h-20 md:h-40">
                 <div>
