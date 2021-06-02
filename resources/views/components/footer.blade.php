7c005334c0d20835995b50baae887e56475e1632
Clayton Stone <devcircus@outlook.com>
2021-06-02 10:01:28 -0500

    Add target=blank to footer links.

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -5,52 +5,52 @@
             <div class="flex flex-col justify-between mt-8 sm:mt-0 sm:w-full sm:px-8 md:flex-row">
                 <div class="flex flex-col">
                     <span class="mb-2 font-bold text-gray-700 uppercase">Favorite Podcasts</span>
-                    <span class="my-2"><a href="https://noplanstomerge.com" class="font-semibold text-indigo-700 text-md hover:text-indigo-500">No Plans To Merge</a></span>
-                    <span class="my-2"><a href="https://phpugly.com" class="font-semibold text-indigo-700 text-md hover:text-indigo-500">PHP Ugly</a></span>
-                    <span class="my-2"><a href="https://laravelpodcast.com" class="font-semibold text-indigo-700 text-md hover:text-indigo-500">The Laravel Podcast</a></span>
-                    <span class="my-2"><a href="https://laravel-news.com/podcast" class="font-semibold text-indigo-700 text-md hover:text-indigo-500">Laravel News Podcast</a></span>
-                    <span class="my-2"><a href="https://northmeetssouth.audio" class="font-semibold text-indigo-700 text-md hover:text-indigo-500">North Meets South</a></span>
-                    <span class="my-2"><a href="https://laracasts.audio" class="font-semibold text-indigo-700 text-md hover:text-indigo-500">The Laracasts Snippet</a></span>
-                    <span class="my-2"><a href="https://mintcast.org" class="font-semibold text-indigo-700 text-md hover:text-indigo-500">mintCast</a></span>
-                    <span class="my-2"><a href="https://fullstackradio.com" class="font-semibold text-indigo-700 text-md hover:text-indigo-500">Full Stack Radio</a></span>
-                    <span class="my-2"><a href="https://tuxdigital.com/thisweekinlinux" class="font-semibold text-indigo-700 text-md hover:text-indigo-500">This Week in Linux</a></span>
-                    <span class="my-2"><a href="https://linuxunplugged.com" class="font-semibold text-indigo-700 text-md hover:text-indigo-500">Linux Unplugged</a></span>
-                    <span class="my-2"><a href="https://darknetdiaries.com" class="font-semibold text-indigo-700 text-md hover:text-indigo-500">Darknet Diaries</a></span>
+                    <span class="my-2"><a href="https://noplanstomerge.com" target="_blank" class="font-semibold text-indigo-700 text-md hover:text-indigo-500">No Plans To Merge</a></span>
+                    <span class="my-2"><a href="https://phpugly.com" target="_blank" class="font-semibold text-indigo-700 text-md hover:text-indigo-500">PHP Ugly</a></span>
+                    <span class="my-2"><a href="https://laravelpodcast.com" target="_blank" class="font-semibold text-indigo-700 text-md hover:text-indigo-500">The Laravel Podcast</a></span>
+                    <span class="my-2"><a href="https://laravel-news.com/podcast" target="_blank" class="font-semibold text-indigo-700 text-md hover:text-indigo-500">Laravel News Podcast</a></span>
+                    <span class="my-2"><a href="https://northmeetssouth.audio" target="_blank" class="font-semibold text-indigo-700 text-md hover:text-indigo-500">North Meets South</a></span>
+                    <span class="my-2"><a href="https://laracasts.audio" target="_blank" class="font-semibold text-indigo-700 text-md hover:text-indigo-500">The Laracasts Snippet</a></span>
+                    <span class="my-2"><a href="https://mintcast.org" target="_blank" class="font-semibold text-indigo-700 text-md hover:text-indigo-500">mintCast</a></span>
+                    <span class="my-2"><a href="https://fullstackradio.com" target="_blank" class="font-semibold text-indigo-700 text-md hover:text-indigo-500">Full Stack Radio</a></span>
+                    <span class="my-2"><a href="https://tuxdigital.com/thisweekinlinux" target="_blank" class="font-semibold text-indigo-700 text-md hover:text-indigo-500">This Week in Linux</a></span>
+                    <span class="my-2"><a href="https://linuxunplugged.com" target="_blank" class="font-semibold text-indigo-700 text-md hover:text-indigo-500">Linux Unplugged</a></span>
+                    <span class="my-2"><a href="https://darknetdiaries.com" target="_blank" class="font-semibold text-indigo-700 text-md hover:text-indigo-500">Darknet Diaries</a></span>
                     <span class="my-4 text-sm text-gray-500 uppercase">Non-Tech Podcasts</span>
-                    <span class="my-2"><a href="https://bretweinstein.net" class="font-semibold text-indigo-700 text-md hover:text-indigo-500">The Dark Horse Podcast</a></span>
-                    <span class="my-2"><a href="https://mikerowe.com/podcast" class="font-semibold text-indigo-700 text-md hover:text-indigo-500">The Way I Heard It</a></span>
-                    <span class="my-2"><a href="https://tim.blog" class="font-semibold text-indigo-700 text-md hover:text-indigo-500">The Tim Ferriss Show</a></span>
+                    <span class="my-2"><a href="https://bretweinstein.net" target="_blank" class="font-semibold text-indigo-700 text-md hover:text-indigo-500">The Dark Horse Podcast</a></span>
+                    <span class="my-2"><a href="https://mikerowe.com/podcast" target="_blank" class="font-semibold text-indigo-700 text-md hover:text-indigo-500">The Way I Heard It</a></span>
+                    <span class="my-2"><a href="https://tim.blog" target="_blank" class="font-semibold text-indigo-700 text-md hover:text-indigo-500">The Tim Ferriss Show</a></span>
                 </div>
                 <div class="flex flex-col">
                     <span class="mt-4 mb-2 font-bold text-gray-700 uppercase md:mt-0">Social Links</span>
                     <span class="my-2">
-                        <a href="https://github.com/devcircus" class="flex font-semibold text-indigo-700 text-md hover:text-indigo-500 group">
+                        <a href="https://github.com/devcircus" target="_blank" class="flex font-semibold text-indigo-700 text-md hover:text-indigo-500 group">
                             <x-bxl-github class="w-4 mr-2 text-indigo-500 group-hover:text-indigo-300" /> Github
                         </a>
                     </span>
                     <span class="my-2">
-                        <a href="https://twitter.com/devcircus" class="flex font-semibold text-indigo-700 text-md hover:text-indigo-500 group">
+                        <a href="https://twitter.com/devcircus" target="_blank" class="flex font-semibold text-indigo-700 text-md hover:text-indigo-500 group">
                             <x-bxl-twitter class="w-4 mr-2 text-indigo-500 group-hover:text-indigo-300" /> Twitter
                         </a>
                     </span>
                     <span class="my-2">
-                        <a href="https://teamkimberlyn.com" class="flex items-center font-semibold text-indigo-700 text-md hover:text-indigo-500 group">
+                        <a href="https://teamkimberlyn.com" target="_blank" class="flex items-center font-semibold text-indigo-700 text-md hover:text-indigo-500 group">
                             <x-bxs-heart class="w-4 mr-2 text-indigo-500 group-hover:text-indigo-300" /> Team Kimberlyn
                         </a>
                     </span>
                 </div>
                 <div class="flex flex-col">
                     <span class="mt-4 mb-2 font-bold text-gray-700 uppercase md:mt-0">Dev Blogs</span>
-                    <span class="my-2"><a href="https://themsaid.com" class="font-semibold text-indigo-700 text-md hover:text-indigo-500">Mohamed Said</a></span>
-                    <span class="my-2"><a href="https://mattstauffer.com/blog/" class="font-semibold text-indigo-700 text-md hover:text-indigo-500">Matt Stauffer</a></span>
-                    <span class="my-2"><a href="https://adamwathan.me" class="font-semibold text-indigo-700 text-md hover:text-indigo-500">Adam Wathan</a></span>
-                    <span class="my-2"><a href="https://blog.laravel.com" class="font-semibold text-indigo-700 text-md hover:text-indigo-500">Laravel Blog</a></span>
-                    <span class="my-2"><a href="https://divinglaravel.com" class="font-semibold text-indigo-700 text-md hover:text-indigo-500">Diving Laravel</a></span>
-                    <span class="my-2"><a href="https://josephsilber.com/posts" class="font-semibold text-indigo-700 text-md hover:text-indigo-500">Joseph Silber</a></span>
-                    <span class="my-2"><a href="https://miguelpiedrafita.com" class="font-semibold text-indigo-700 text-md hover:text-indigo-500">Miguel Piedrafita</a></span>
-                    <span class="my-2"><a href="https://marcelpociot.de" class="font-semibold text-indigo-700 text-md hover:text-indigo-500">Marcel Pociot</a></span>
-                    <span class="my-2"><a href="https://aschmelyun.com/" class="font-semibold text-indigo-700 text-md hover:text-indigo-500">Andrew Schmelyun</a></span>
-                    <span class="my-2"><a href="https://calebporzio.com" class="font-semibold text-indigo-700 text-md hover:text-indigo-500">Caleb Porzio</a></span>
+                    <span class="my-2"><a href="https://themsaid.com" target="_blank" class="font-semibold text-indigo-700 text-md hover:text-indigo-500">Mohamed Said</a></span>
+                    <span class="my-2"><a href="https://mattstauffer.com/blog/" target="_blank" class="font-semibold text-indigo-700 text-md hover:text-indigo-500">Matt Stauffer</a></span>
+                    <span class="my-2"><a href="https://adamwathan.me" target="_blank" class="font-semibold text-indigo-700 text-md hover:text-indigo-500">Adam Wathan</a></span>
+                    <span class="my-2"><a href="https://blog.laravel.com" target="_blank" class="font-semibold text-indigo-700 text-md hover:text-indigo-500">Laravel Blog</a></span>
+                    <span class="my-2"><a href="https://divinglaravel.com" target="_blank" class="font-semibold text-indigo-700 text-md hover:text-indigo-500">Diving Laravel</a></span>
+                    <span class="my-2"><a href="https://josephsilber.com/posts" target="_blank" class="font-semibold text-indigo-700 text-md hover:text-indigo-500">Joseph Silber</a></span>
+                    <span class="my-2"><a href="https://miguelpiedrafita.com" target="_blank" class="font-semibold text-indigo-700 text-md hover:text-indigo-500">Miguel Piedrafita</a></span>
+                    <span class="my-2"><a href="https://marcelpociot.de" target="_blank" class="font-semibold text-indigo-700 text-md hover:text-indigo-500">Marcel Pociot</a></span>
+                    <span class="my-2"><a href="https://aschmelyun.com/" target="_blank" class="font-semibold text-indigo-700 text-md hover:text-indigo-500">Andrew Schmelyun</a></span>
+                    <span class="my-2"><a href="https://calebporzio.com" target="_blank" class="font-semibold text-indigo-700 text-md hover:text-indigo-500">Caleb Porzio</a></span>
                 </div>
             </div>
         </div>
