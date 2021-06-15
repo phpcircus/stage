2947eca49ceb60b88c1d9ac58f694abd6d95ec97
Clayton Stone <devcircus@outlook.com>
2021-06-15 09:46:49 -0500

    Upgrade to alpine v3 and address issues from the upgrade.

--- resources/views/components/input/rich-text.blade.php
+++ resources/views/components/input/rich-text.blade.php
@@ -1,4 +1,4 @@
-<div class="rounded-md shadow-sm" x-data="richText()" x-init="init($watch)"
+<div class="rounded-md shadow-sm" x-data="richText"
     x-on:trix-change="setValueOnChange($event)" {{ $attributes->whereDoesntStartWith('wire:model') }} wire:ignore>
     <input id="x" type="hidden" tabindex="0">
     <trix-editor id="trix" x-ref="trix" input="x" style="min-height: 350px;"
@@ -6,20 +6,20 @@
 
     @push('scripts')
         <script>
-            function richText() {
-                return {
+            document.addEventListener('alpine:initializing', function () {
+                Alpine.data('richText', () => ({
                     value: @entangle($attributes->wire('model')),
                     isFocused() { return document.activeElement !== document.getElementById('trix') },
                     setValue() { document.getElementById('trix').editor.loadHTML(this.value) },
-                    init(watch) {
+                    init() {
                         this.setValue();
-                        watch('value', () => this.isFocused() && this.setValue());
+                        this.$watch('value', () => this.isFocused() && this.setValue());
                     },
                     setValueOnChange(event) {
                         this.value = event.target.value;
                     }
-                }
-            }
+                }));
+            });
         </script>
     @endpush
 </div>
