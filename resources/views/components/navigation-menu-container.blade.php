2ecc9c1266c5a33d23ebaeddcafa1d5f0e933d0b
Clayton Stone <devcircus@outlook.com>
2021-06-21 11:32:19 -0500

    Fixes to nav layout and page hero.

--- resources/views/components/navigation-menu-container.blade.php
+++ resources/views/components/navigation-menu-container.blade.php
@@ -1,6 +1,6 @@
 <div x-data class="fixed top-0 z-50 w-full bg-white shadow-md dark:bg-gray-800">
-    <header class="font-semibold text-gray-900">
-        <div class="relative hidden lg:py-4 lg:px-6 xl-px-0 lg:flex lg:mx-auto lg:max-w-7xl lg:mb-4">
+    <header class="flex items-center mx-auto font-semibold text-gray-900 lg:max-w-7xl">
+        <div class="relative hidden w-full lg:py-4 lg:px-6 xl:px-0 lg:flex lg:mb-4 lg:justify-between">
             <a href="{{ route('home') }}">
                 <img x-cloak x-show="! $store.stage.darkMode" src="/img/phpstage.png" class="h-16 mb-8 mr-0 lg:mb-0 lg:mr-24" />
                 <img x-cloak x-show="$store.stage.darkMode" src="/img/phpstage_white.png" class="h-16 mb-8 mr-0 lg:mb-0 lg:mr-24" />
@@ -10,7 +10,11 @@
             <livewire:navigation-main />
 
             <!-- Settings Dropdown -->
-            <div>
+            <div class="flex items-center ml-auto">
+                <div x-cloak class="mr-4 cursor-pointer" x-on:click="$store.stage.toggleTheme()">
+                    <x-heroicon-o-moon x-cloak x-show="$store.stage.darkMode" class="w-5 text-white"></x-heroicon-o-moon>
+                    <x-heroicon-o-sun x-cloak x-show="! $store.stage.darkMode" class="w-5 text-gray-900"></x-heroicon-o-sun>
+                </div>
                 <x-nav-settings-dropdown />
             </div>
         </div>
@@ -18,12 +22,16 @@
         <div class="relative flex flex-col justify-between px-6 py-4 mx-auto lg:hidden">
             <div class="flex justify-between">
                 <a href="{{ route('home') }}">
-                    <img x-cloak x-show="! $store.stage.darkMode" src="/img/phpstage.png" class="mb-8 mr-0 xxs:h-8 xs:h-12" />
-                    <img x-cloak x-show="$store.stage.darkMode" src="/img/phpstage_white.png" class="mb-8 mr-0 xxs:h-8 xs:h-12" />
+                    <img x-cloak x-show="! $store.stage.darkMode" src="/img/phpstage.png" class="mb-4 mr-0 xxs:h-8 xs:h-12" />
+                    <img x-cloak x-show="$store.stage.darkMode" src="/img/phpstage_white.png" class="mb-4 mr-0 xxs:h-8 xs:h-12" />
                 </a>
 
                 <!-- Settings Dropdown -->
-                <div>
+                <div class="flex items-center ml-auto">
+                    <div x-cloak class="mr-4 cursor-pointer" x-on:click="$store.stage.toggleTheme()">
+                        <x-heroicon-o-moon x-cloak x-show="$store.stage.darkMode" class="w-5 text-white"></x-heroicon-o-moon>
+                        <x-heroicon-o-sun x-cloak x-show="! $store.stage.darkMode" class="w-5 text-gray-900"></x-heroicon-o-sun>
+                    </div>
                     <x-nav-settings-dropdown />
                 </div>
             </div>
