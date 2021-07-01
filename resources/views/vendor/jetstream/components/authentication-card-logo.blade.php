771c71291b7f953c90dcb306283f39075e96d2b7
Clayton Stone <devcircus@outlook.com>
2021-06-30 19:34:02 -0500

    Fix css prod mode not working. Replace site logo on login page.

--- resources/views/vendor/jetstream/components/authentication-card-logo.blade.php
+++ resources/views/vendor/jetstream/components/authentication-card-logo.blade.php
@@ -1,3 +1,4 @@
-<a href="/">
-    <img src="/img/phpstage.png" class="h-12">
-</a>
+<div class="flex items-center flex-shrink-0 mr-2 md:mr-8">
+    <img src="/img/dark_elephant.png" class="h-6 mr-1" />
+    <span class="text-xl text-transparent uppercase bg-gradient-to-r xs:text-2xl sm:text-3xl bg-clip-text from-gray-600 to-gray-400 font-soloist">PhpStage</span>
+</div>
