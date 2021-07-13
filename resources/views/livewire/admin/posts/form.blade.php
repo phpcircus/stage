6903156ebbeca1050c57f561c628b2db265c4f1f
Clayton Stone <devcircus@outlook.com>
2021-07-12 20:41:10 -0500

    UI tweaks to login form, buttons, date input, lightmode bg colors.

--- resources/views/livewire/admin/posts/form.blade.php
+++ resources/views/livewire/admin/posts/form.blade.php
@@ -54,8 +54,10 @@
             <x-button.link wire:click="cancel" class="mr-4">
                 Cancel
             </x-button.link>
-            <x-button.primary wire:click.prevent="save" class="bg-indigo-400 dark:bg-indigo-500">
+            <x-button.primary wire:click.prevent="save" class="!py-[.6rem] !text-white !bg-indigo-500 !ring hover:!ring-offset-2 ring-indigo-600 dark:!ring-indigo-400">
                 Save
+                <x-heroicon-o-chevron-right class="h-4 ml-2 text-white group-hover:hidden"></x-heroicon-chevron-right>
+                <x-heroicon-o-arrow-right class="hidden h-4 ml-2 text-white group-hover:inline-block"></x-heroicon-chevron-right>
             </x-button.primary>
         </div>
     </div>
