3436e9dc350a70aabf35964190d8137fc5da0705
Clayton Stone <devcircus@outlook.com>
2021-07-28 21:12:05 -0500

    Remove the solid bgs on about & projects.

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,43 +1,49 @@
 <x-app-layout>
     <div class="px-4 py-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
         <x-page-hero>
-            <h1 id="page-title" class="text-4xl font-bold uppercase text-skin-quiet sm:text-6xl font-lemonmilk">About Me</h1>
-        </x-page-header>
-        <div class="pb-6 mx-auto lg:pb-8">
-            <div class="flex flex-col p-4 rounded-lg shadow-md lg:p-6 bg-skin-fill-mantle/50">
-                <div x-data="{ show: false }" x-init="setTimeout(function(){ show = true; }, 500)" class="flex flex-col lg:flex-row">
-                    <div class="flex flex-col w-full mb-4 space-y-2 lg:w-1/3 lg:mb-0">
-                        <h1 class="mb-4 text-3xl font-bold lg:text-5xl font-protogrotesk text-skin-quiet">
-                            {{ config('auth.admin.name') }}
-                        </h1>
-                        <span class="font-sans text-2xl font-semibold text-skin-loud">
-                            Years in web development:
-                            <span class="text-xl text-skin-muted">
-                                 16
+            <h1 id="page-title" class="text-4xl font-bold uppercase text-skin-quiet sm:text-6xl font-lemonmilk">About Me
+            </h1>
+            </x-page-header>
+            <div class="pb-6 mx-auto lg:pb-8">
+                <div class="flex flex-col p-4 lg:p-6">
+                    <div x-data="{ show: false }" x-init="setTimeout(function(){ show = true; }, 500)"
+                        class="flex flex-col lg:flex-row">
+                        <div class="flex flex-col w-full mb-4 space-y-2 lg:w-1/3 lg:mb-0">
+                            <h1 class="mb-4 text-3xl font-bold lg:text-5xl font-protogrotesk text-skin-quiet">
+                                {{ config('auth.admin.name') }}
+                            </h1>
+                            <span class="font-sans text-2xl font-semibold text-skin-loud">
+                                Years in web development:
+                                <span class="text-xl text-skin-muted">
+                                    16
+                                </span>
                             </span>
-                        </span>
-                        <span class="font-sans text-2xl font-semibold text-skin-loud">
-                            Started with:
-                            <span class="text-xl text-skin-muted">
-                                 Coldfusion
+                            <span class="font-sans text-2xl font-semibold text-skin-loud">
+                                Started with:
+                                <span class="text-xl text-skin-muted">
+                                    Coldfusion
+                                </span>
                             </span>
-                        </span>
-                        <span class="font-sans text-2xl font-semibold text-skin-loud">
-                            Current:
-                            <span class="text-xl text-skin-muted">
-                                 PHP(Laravel), Vue, Livewire, Alpinejs
+                            <span class="font-sans text-2xl font-semibold text-skin-loud">
+                                Current:
+                                <span class="text-xl text-skin-muted">
+                                    PHP(Laravel), Vue, Livewire, Alpinejs
+                                </span>
                             </span>
-                        </span>
+                        </div>
+                        <img x-show="show" x-cloak src="/img/clay_kim.jpg"
+                            class="object-cover object-bottom w-64 h-64 border-4 border-red-600 rounded-full shadow-md lg:ml-8"
+                            x-transition:enter="transition ease-out duration-1000"
+                            x-transition:enter-start="filter grayscale transform scale-0"
+                            x-transition:enter-end="filter grayscale-0 transform scale-100" />
                     </div>
-                    <img x-show="show" x-cloak src="/img/clay_kim.jpg" class="object-cover object-bottom w-64 h-64 border-4 border-red-600 rounded-full shadow-md lg:ml-8"
-                        x-transition:enter="transition ease-out duration-1000"
-                        x-transition:enter-start="filter grayscale transform scale-0"
-                        x-transition:enter-end="filter grayscale-0 transform scale-100" />
+                    <p
+                        class="mt-8 font-sans text-xl font-semibold leading-relaxed text-skin-base first-letter:font-solo first-letter:text-2xl first-letter:italic first-letter:text-skin-muted">
+                        I've started this blog to track my experiences and to have a place to log what I'm learning. So
+                        this is more of a tool for me than anything else. However, maybe I'll write something that can
+                        be useful to others. Either way, I'm excited about what is to come.
+                    </p>
                 </div>
-                <p class="mt-8 font-sans text-xl leading-relaxed text-skin-base first-letter:font-badhandwriting first-letter:text-2xl first-letter:italic first-letter:text-skin-muted">
-                    I've started this blog to track my experiences and to have a place to log what I'm learning. So this is more of a tool for me than anything else. However, maybe I'll write something that can be useful to others. Either way, I'm excited about what is to come.
-                </p>
             </div>
-        </div>
     </div>
 </x-app-layout>
